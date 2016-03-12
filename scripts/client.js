84f2f2696d8d058b7357585dd07772f5a5cbb3c0
Akil Harris <akil.harris@gmail.com>
2016-03-12 13:40:55 -0500

    initial commit

---
+++ scripts/client.js
@@ -0,0 +1,9 @@
+import {default as React} from "react";
+import {default as ReactDOM} from "react-dom";
+
+import {default as ReactRoot} from "./ReactRoot";
+
+import {} from "../styles/reset.css";
+import {} from "../styles/fonts.css";
+
+ReactDOM.render(<ReactRoot />, document.getElementById("react-container"));
